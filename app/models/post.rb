b7478ea912d8b273ef8e0e60f62a3aadf995a15a
EC2 Default User <ec2-user@ip-172-31-10-207.ap-northeast-1.compute.internal>
2020-09-10 18:07:05 +0900

    change like button 参加

---
+++ app/models/post.rb
@@ -0,0 +1,12 @@
+class Post < ApplicationRecord
+  validates :content, {presence: true, length: {maximum: 140}}
+  validates :user_id, {presence: true}
+  
+  def user
+    return User.find_by(id: self.user_id)
+  end
+  
+  belongs_to :user
+  has_many :comments
+  
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+     
+     has_secure_password
+     
+     validates :name, {presence: true}
+     validates :email, {presence: true, uniqueness: true}
+     
+     def posts
+     return Post.where(user_id: self.id)
+     end
+     
+     has_many :posts, :dependent => :destroy
+     has_many :comments
+     
+end
